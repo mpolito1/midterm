9d640b44133f0a149f47875fd3b92d863c9a9dc2
mpolito1 <mpolito1@pride.hofstra.edu>
2025-03-30 18:43:36 -0400

    midterm in progress

---
+++ script.js
@@ -0,0 +1,13 @@
+//  color array for background of the main part of the body 
+const colors = ["#fff9f7", "#fefff7", "#f7fff7"]; // grey, yellow, green
+
+console.log("The first color in the array is:", colors[0]);
+//const userName = prompt("What is your name?");
+//const colorChoice = prompt("Pick a background color: 0 for grey, 1 for yellow, 2 for green");
+const userInfo = {
+    name: userName,
+    selectedColor: colors[colorChoice]
+};
+
+//document.body.style.backgroundColor = userInfo.selectedColor;
+document.body.style.backgroundColor = "#fff9f7";
